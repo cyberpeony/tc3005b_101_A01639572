7ec3df9eacbae2e51af99461bd1918a464e3748d
Fernanda <a01639572@tec.mx>
2025-03-31 08:05:19 -0600

    TDD controller tests

---
+++ greetingController/src/main/java/com/example/greetingController/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.greetingController.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.greetingController.model.Greeting;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(String.format(template, name));
+    }
+}
---
+++ greetingController/src/main/java/com/example/greetingController/model/Greeting.java
@@ -0,0 +1,6 @@
+package com.example.greetingController.model;
+
+public record Greeting(String greeting) {
+
+}
+
