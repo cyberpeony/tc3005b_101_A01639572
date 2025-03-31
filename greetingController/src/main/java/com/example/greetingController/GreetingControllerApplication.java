0eec7c54df06f9ef11ef9fcb51d8d57fbdad9a31
Fernanda <a01639572@tec.mx>
2025-03-31 07:25:31 -0600

    springboot project

---
+++ greetingController/src/main/java/com/example/greetingController/GreetingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingControllerApplication.class, args);
+	}
+
+}
